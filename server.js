c1d846aca08175477a758dbfd01cf0341dd45e33
uniorunr <gagael23@live.com>
2019-08-29 16:33:30 +0300

    feat: send message after connection

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 3000 });
 
 server.on('connection', (ws) => {
-  console.log('Hi there! Client connected!');
+  server.send('Hi from server!');
   ws.on('close', () => console.log('Client disconnected :('));
 });
