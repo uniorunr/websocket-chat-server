241ca16e3a83c14574d8158f71f8646a920ed35f
uniorunr <gagael23@live.com>
2019-08-29 16:57:57 +0300

    feat: update ws listeners

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 3000 });
 
 server.on('connection', (ws) => {
-  server.send('Hi from server!');
+  ws.send('Hi from server!');
   ws.on('close', () => console.log('Client disconnected :('));
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
 });
