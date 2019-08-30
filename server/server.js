5bc04b91c07b04cfb21149867e0d3a0d4c0b4535
uniorunr <gagael23@live.com>
2019-08-30 11:19:14 +0300

    feat: implement ping pong checkers

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const app = express();
-
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', ws => {
+  ws.isAlive = true;
+
+  ws.on('pong', () => {
+    ws.isAlive = true;
+  });
+
   ws.on('message', message => {
     wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
@@ -16,6 +21,16 @@ wss.on('connection', ws => {
     });
   });
 });
+
+setInterval(() => {
+  wss.clients.forEach(ws => {
+
+    if (!ws.isAlive) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(null, false, true);
+  });
+}, 10000);
 
 server.listen(process.env.PORT || 8080, () => {
   console.log(`Port ${server.address().port}`);
